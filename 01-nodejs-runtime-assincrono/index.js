d5002584b0236c0a609dc1cc095617eca5b00cbf
Juliane Cardoso <julianesilvac75@gmail.com>
2024-06-17 22:43:14 +0100

    initial commit

---
+++ 01-nodejs-runtime-assincrono/index.js
@@ -0,0 +1,17 @@
+const { brl, usdToBrl } = require('./brlValue');
+
+// const brlValue = require('./brlValue');
+// const convert = require('./brlValue');
+
+// const usd = 10;
+// const brl = convert(usd);
+
+// console.log(brl);
+
+// console.log(brlValue);
+
+// console.log(`Valor do dólar: ${brlValue.brl}`);
+// console.log(`10 dólares em reais: ${brlValue.usdToBrl(10)}`);
+
+console.log(`Valor do dólar: ${brl}`);
+console.log(`10 dólares em reais: ${usdToBrl(10)}`);
